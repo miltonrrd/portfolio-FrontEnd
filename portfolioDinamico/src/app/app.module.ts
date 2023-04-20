104d3ccc43c0f3fe507c9948d064c5bdf258a591
miltonrrd <milton-rd@hotmail.com>
2023-04-20 00:23:29 -0300

    se agrego componente footer

--- portfolioDinamico/src/app/app.module.ts
+++ portfolioDinamico/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardysoftskillsComponent } from './components/hardysoftskills/hardysoftskills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     EducacionComponent,
     HardysoftskillsComponent,
     ProyectosComponent,
+    FooterComponent,
     
   ],
   imports: [
