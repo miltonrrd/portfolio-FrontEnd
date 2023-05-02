9ae347bc2c086038227735dfb180f7b9f03bfd13
miltonrrd <milton-rd@hotmail.com>
2023-05-02 18:32:58 -0300

    Se agrego ruteo home y login

--- portfolioDinamico/src/app/app.module.ts
+++ portfolioDinamico/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { HardysoftskillsComponent } from './components/hardysoftskills/hardysoft
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router'; 
 
 @NgModule({
   declarations: [
@@ -27,6 +31,8 @@ import { HttpClientModule } from '@angular/common/http';
     HardysoftskillsComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
     
   ],
   imports: [
@@ -49,7 +55,11 @@ import { HttpClientModule } from '@angular/common/http';
       "clockwise": false,
       "startFromZero": false,
       "lazy": true}),
-      HttpClientModule
+      HttpClientModule,
+      AppRoutingModule,
+      RouterModule.forRoot([
+        { path: '', component: HomeComponent },
+        { path: 'login', component: LoginComponent }])
   ],
   providers: [],
   bootstrap: [AppComponent],
--- portfolioDinamico/src/app/components/button/button.component.ts
+++ portfolioDinamico/src/app/components/button/button.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent implements OnInit {
 
-  constructor() { }
+export class ButtonComponent implements OnInit {
+  constructor(private router:Router) { }
+login() {
+  this.router.navigate(['/login']);
+}
 
   ngOnInit(): void {
+   
   }
 
 }
