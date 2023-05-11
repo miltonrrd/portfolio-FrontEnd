7d6a0cfc88339d9c14f139556b0a2311d434665f
miltonrrd <milton-rd@hotmail.com>
2023-05-10 22:33:16 -0300

    se implemento login con jwt en el front

--- portfolioDinamico/src/app/app-routing.module.ts
+++ portfolioDinamico/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes= [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- portfolioDinamico/src/app/app.module.ts
+++ portfolioDinamico/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router'; 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -56,10 +57,9 @@ import { RouterModule } from '@angular/router';
       "startFromZero": false,
       "lazy": true}),
       HttpClientModule,
-      AppRoutingModule,
-      RouterModule.forRoot([
-        { path: '', component: HomeComponent },
-        { path: 'login', component: LoginComponent }])
+      FormsModule,
+      AppRoutingModule
+      
   ],
   providers: [],
   bootstrap: [AppComponent],
